0877569f4c50fdf3e7194d8f792a624feba25178
jonjon5 <jonjonlacourt@live.be>
2025-08-08 12:37:30 +0200

    Configured project for PostgreSQL and dj-database-url

--- GestionClient/settings.py
+++ GestionClient/settings.py
@@ -82,11 +82,22 @@ WSGI_APPLICATION = 'GestionClient.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+"""
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 # Password validation
